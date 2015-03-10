3924c410aa5bf4832c1b2968eff3177c622d777f
Gennady Karev <pendolf666@gmail.com>
2015-03-10 23:07:02 +0600

    Create a session model.

---
+++ board/static/board/js/models.js
@@ -0,0 +1,35 @@
+(function ($, Backbone, _, app) {
+
+    var Session = Backbone.Model.extend({
+        defaults: {
+            token: null
+        },
+        initialize: function (options) {
+            this.options = options;
+            this.load();
+        },
+        load: function () {
+            var token = localStorage.apiToken;
+            if (token) {
+                this.set('token', token);
+            }
+        },
+        save: function (token) {
+            this.set('token', token);
+            if (token === null) {
+                localStorage.removeItem('apiToken');
+            } else {
+                localStorage.apiToken = token;
+            }
+        },
+        delete: function () {
+            this.save(null);
+        },
+        authenticated: function () {
+            return this.get('token') !== null;
+        }
+    });
+
+    app.session = new Session();
+
+})(jQuery, Backbone, _, app);
